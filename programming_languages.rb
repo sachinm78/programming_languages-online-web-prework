5662c2e947994fcab1c6307932f5f996c5ce1139
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 18:54:10 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -32,5 +32,13 @@ languages = {
 
 
 def reformat_languages(languages)
-  # your code here
+new_languages = {}
+languages.each do |style, language|
+  language.each do |name, attributes|
+  new_languages[name] ||= attributes
+  new_languages[name][:style]=[]
+  new_languages[name][:style]<<style
+end
+end
+new_languages
 end
